8da8cae2ac593fda71ea0bb4bccb30d5ba64fe8f
testusuke <silverbro007@icloud.com>
2021-09-19 00:28:47 +0900

    feat: add listener bot

--- src/listener_client.js
+++ src/listener_client.js
@@ -1,8 +1,33 @@
+import {Client, Intents} from "discord.js";
 
-class ListenerClient extends Client {
-    constructor() {
-        super();
+class ListenerClient {
+    constructor(token) {
+        this.client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+        //  event
+        this.client.on('messageCreate', async msg => this.onMessage(msg));
+
+        //  login
+        this.client.login(token).then(r => {
+            this.enabled = true;
+        });
     }
+
+    onMessage(msg) {
+        if (this.enabled === false) return; //  is enabled
+        if (msg.system || msg.author.bot || msg.author.system) return;  //  is user
+        if (msg.content.length < 1) return; //  is empty
+
+        //  prefix
+        if (msg.content.startsWith('^')){
+
+        }
+        //  track message
+        else {
+
+        }
+
+    }
+
 }
 
-export default ListenerClient
+export default ListenerClient;
