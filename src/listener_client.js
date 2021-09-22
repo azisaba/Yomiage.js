dac1654b6abdf8c71eafc91101a04601bd5da57a
testusuke <silverbro007@icloud.com>
2021-09-22 21:11:00 +0900

    change: add base command

--- src/listener_client.js
+++ src/listener_client.js
@@ -1,8 +1,8 @@
-import {Client, Intents} from "discord.js";
+import {Client, Intents, Util} from "discord.js";
 
 class ListenerClient {
     constructor(token) {
-        this.client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+        this.client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
         //  event
         this.client.on('messageCreate', async msg => this.onMessage(msg));
 
@@ -18,11 +18,48 @@ class ListenerClient {
         if (msg.content.length < 1) return; //  is empty
 
         //  prefix
-        if (msg.content.startsWith('^')){
+        if (msg.content.startsWith('^')) {
+            //  remove prefix and split content
+            const args = msg.content.slice(1).split(' ');
+            //  safety
+            if (args.length < 1) return;
+
+            switch (args[0]) {
+                //  connect
+                case 'con':
+
+                    break
+
+                //  disconnect
+                case 'dc':
+
+                    break
+
+                //  dictionary
+                case 'dict':
+
+                    break
+
+                //  status
+                case 'status':
+
+                    break
+
+                //  help
+                case "help":
+
+                    break
+
+                //  other
+                default:
+                    break
+            }
 
         }
         //  track message
         else {
+            //  Replace a mention to text, Remove Code-Block-Content, and Remove url
+            let cleanedMessage = Util.cleanCodeBlockContent(msg.cleanContent).replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
 
         }
 
