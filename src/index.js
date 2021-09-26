97b9b661f3afdba06925394cea14303431979430
testusuke <silverbro007@icloud.com>
2021-09-26 22:48:11 +0900

    change: add some initializer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import Config from "./config";
-
+//  read environment valuables
+const dataDirectoryPath = process.env.DATA_DIRECTORY;
 const configPath = process.env.CONFIG_PATH;
+
+//  load configuration
+const Config = require('./config.js');
 const config = new Config(configPath);
+const configData = config.data;
+console.log(configData.token);
 
+//  init dictionary
+const Dict = require('./dict.js');
+const dict = new Dict(`${dataDirectoryPath}/dict/`);
 
+//  launch clients
+
+//  run thread
