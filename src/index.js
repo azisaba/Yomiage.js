00ab03c21e4cab9b73b85ae70839cac5156ee0a4
testusuke <silverbro007@icloud.com>
2021-12-20 19:20:00 +0900

    feat: add dict. add listener token

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
+const Config = require('./config.js');
+const Dict = require('./dict.js');
+const ListenerClient = require('./listener_client.js');
+const SpeakerClient = require('./speaker_client.js');
+
+
+///////////////////////////
 //  read environment valuables
 const dataDirectoryPath = process.env.DATA_DIRECTORY;
 const configPath = process.env.CONFIG_PATH;
 
+
+///////////////////////////
 //  load configuration
-const Config = require('./config.js');
 const config = new Config(configPath);
 const configData = config.data;
 console.log(configData.token);
 
+
+///////////////////////////
 //  init dictionary
-const Dict = require('./dict.js');
-const dict = new Dict(`${dataDirectoryPath}/dict/`);
+const dict = new Dict(`${dataDirectoryPath}`);
 
+
+///////////////////////////
 //  launch clients
+const listener_token = configData.token.listener;
+const speaker_tokens = configData.token.speaker;
 
-//  run thread
+//  check exception
+if (speaker_tokens.length <= 0) {
+    console.log("Listener bot must be more than 1 token.");
+    process.exit(0);
+}
+
+//  launch listener client
+const listener = new ListenerClient(listener_token, dict);
+
+//  launch speaker client
+const speakers = [];
+speaker_tokens.forEach((token) => {
+    const speaker = new SpeakerClient(token);
+    //  insert instance
+    speakers.push(speaker);
+});
--- src/listener_client.js
+++ src/listener_client.js
@@ -1,10 +1,14 @@
-import {Client, Intents, Util} from "discord.js";
+const {Client, Intents, Util} = require('discord.js');
 
 class ListenerClient {
-    constructor(token) {
+    constructor(token, dict) {
+        //  dict
+        this.dict = dict;
+        //  client
         this.client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
         //  event
         this.client.on('messageCreate', async msg => this.onMessage(msg));
+        this.client.on('voiceStateUpdate', async (oldState, newState) => this.onLeaveVoiceChat(oldState, newState));
 
         //  login
         this.client.login(token).then(r => {
@@ -27,32 +31,74 @@ class ListenerClient {
             switch (args[0]) {
                 //  connect
                 case 'con':
-
-                    break
+                    //  TODO Request to connect
+                    break;
 
                 //  disconnect
                 case 'dc':
-
-                    break
+                    //  TODO Request to disconnect
+                    break;
 
                 //  dictionary
                 case 'dict':
+                    //  length
+                    if (args.length < 2) {
+                        msg.channel.send(":boom:エラー:構文が不正です。");
+                        break;
+                    }
 
-                    break
+                    switch (args[1]) {
+                        case 'add':
+                            if (args.length < 4) {
+                                msg.channel.send(":boom:エラー:構文が不正です。");
+                                break;
+                            }
+                            //  add
+                            this.dict.add(args[2], args[3]);
+                            msg.channel.send(`これからは ${args[2]} を ${args[2]} と読みます！`);
+                            break;
+
+                        case 'remove':
+                            if (args.length < 3) {
+                                msg.channel.send(":boom:エラー:構文が不正です。");
+                                break;
+                            }
+                            //  add
+                            this.dict.remove(args[2]);
+                            msg.channel.send(`${args[2]} を辞書から削除しました`);
+                            break;
+
+                        default:
+                            msg.channel.send(":boom:エラー:コマンドが不正です。ヘルプで確認してください。^help");
+                            break
+                    }
+
+                    break;
 
                 //  status
                 case 'status':
 
-                    break
+                    break;
 
                 //  help
                 case "help":
-
-                    break
+                    msg.channel.send({
+                        embeds: {
+                            title: ':question: ヘルプ',
+                            description:
+                                "Command:\n" +
+                                "- ^con : 読み上げを開始します\n" +
+                                "- ^dc : 切断します\n" +
+                                "- ^status : ステータスを表示します\n" +
+                                "- ^dict add/remove <A> <B> : AをBと呼ぶ辞書の追加/削除\n" +
+                                "- ^help : ヘルプを表示します"
+                        }
+                    });
+                    break;
 
                 //  other
                 default:
-                    break
+                    break;
             }
 
         }
@@ -60,11 +106,19 @@ class ListenerClient {
         else {
             //  Replace a mention to text, Remove Code-Block-Content, and Remove url
             let cleanedMessage = Util.cleanCodeBlockContent(msg.cleanContent).replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-
+            cleanedMessage = this.dict.replace(cleanedMessage);
         }
 
     }
 
+    onLeaveVoiceChat(oldState, newState) {
+        //  TODO confirm whatever channel is tracked
+
+        //  confirm whatever only bot is in channel
+        if (newState.member.length === 1) {
+            //  TODO speaker leave implementation
+        }
+    }
 }
 
 module.exports =  ListenerClient;
