e803d9e1d1e7566d9b4d6bc2c8d15bd7c90524b2
testusuke <silverbro007@icloud.com>
2021-12-21 21:25:22 +0900

    feat: add shutdown and interval callback

--- src/speaker_client.js
+++ src/speaker_client.js
@@ -1,15 +1,51 @@
-import {Client, Intents} from "discord.js";
+const {Client, Intents} = require('discord.js');
 
 class SpeakerClient {
+    static INTERVAL_TIME_MS = 1000;
 
     constructor(token) {
         this.client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
         //  login
         this.client.login(token).then(r => {
             this.enabled = true;
+
+            //  set interval
+            this._timer = setInterval(() => this._intervalCallback(), SpeakerClient.INTERVAL_TIME_MS);
         });
     }
 
+    /**
+     * Shutdown client and Clear interval
+     */
+    shutdown() {
+        console.log('shutdown bot...');
+        //  disable
+        this.enabled = false;
+    }
+
+    addMessageQueue() {
+
+    }
+
+    /**
+     * Interval callback of reading message
+     * @private
+     */
+    _intervalCallback() {
+
+        //  check shutdown
+        if(!this.enabled) {
+            //  cancel
+            clearInterval(this._timer);
+            //  logout
+            this.client.destroy();
+
+            console.log('shutdown done.');
+        }
+        //  TODO read message in voice channel
+
+    }
 }
 
 module.exports =  SpeakerClient;
