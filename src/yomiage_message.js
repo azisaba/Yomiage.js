f0c6e9df3893f65417b3414ec4fd168cef9513ec
testusuke <silverbro007@icloud.com>
2022-02-17 16:19:48 +0900

    change: add guild variable

--- src/yomiage_message.js
+++ src/yomiage_message.js
@@ -1,10 +1,12 @@
 /**
  * Message class contains channel, message. this message will be read in vc.
+ * @param {String} guild Guild id
  * @param {String} channel Voice Channel id
  * @param {String} message Message (recommend message is cleaned)
  * @constructor
  */
-const YomiageMessage = (channel, message) => {
+const YomiageMessage = (guild, channel, message) => {
+    this.guild = guild;
     this.channel = channel;
     this.message = message;
 }
